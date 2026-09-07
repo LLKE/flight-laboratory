f806e6394596eb024a1bf2024cd9b1071ffec732
Laurens Keuker <laurens.keuker@gmail.com>
2026-09-07 21:58:57 +0200

    redesign (timer): add isElapsed function to timer class

--- src/util/timer.h
+++ src/util/timer.h
@@ -4,22 +4,21 @@
 #include <chrono>
 
 class Timer {
-private:
-    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
 
 public:
     Timer() {
-        start_time = std::chrono::high_resolution_clock::now();
+        _start_time = std::chrono::high_resolution_clock::now();
     }
 
-    ~Timer() {
-        auto end_time = std::chrono::high_resolution_clock::now();
-        auto start = std::chrono::time_point_cast<std::chrono::microseconds>(start_time).time_since_epoch().count();
-        auto end = std::chrono::time_point_cast<std::chrono::microseconds>(end_time).time_since_epoch().count();
-        
-        auto duration = end - start;
-        double ms = duration * 0.001; 
-    }
+    ~Timer() {}
+
+    bool isElapsed(int num_seconds) {
+        std::chrono::duration<double> elapsed_seconds = std::chrono::high_resolution_clock::now() - _start_time;
+        return elapsed_seconds >= std::chrono::milliseconds(num_seconds * 1000);
+    };
+
+private: 
+    std::chrono::time_point<std::chrono::high_resolution_clock> _start_time;
 };
 
 #endif // TIMER_H
