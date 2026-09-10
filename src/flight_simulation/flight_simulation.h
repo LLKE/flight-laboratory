594c1ff274509d8942eb43b25a4bdcd5473e6f5e
Laurens Keuker <laurens.keuker@gmail.com>
2026-09-10 19:39:02 +0200

    fix (flight_simulation): Getting stuff to build here as well

--- src/flight_simulation/flight_simulation.h
+++ src/flight_simulation/flight_simulation.h
@@ -1,35 +1,36 @@
-#ifndef FLIGHT_SIMULAITON_H
+#ifndef FLIGHT_SIMULATION_H
 #define FLIGHT_SIMULATION_H
 
-#include <memory> 
+#include <memory>
 
 #include <QObject>
 #include <QTimer>
 
 #include "src/control/loops/control_loop.h"
 
-class FlightSimulation 
+class FlightSimulation : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY( pitch READ pitch NOTIFY pitchChanged())
+    Q_PROPERTY(double pitch READ pitch NOTIFY pitchChanged)
 
 public:
-    explicit FlightSimulation(std::unique_ptr<ControlLoop> loop, double dt, QObject *parent = nullptr);
+    explicit FlightSimulation(
+        std::unique_ptr<ControlLoop> loop,
+        double dt,
+        QObject *parent = nullptr);
 
-    float pitch() const { return _pitch; };
+    double pitch() const { return _pitch; }
 
 signals:
     void pitchChanged(double newPitch);
 
 private:
     std::unique_ptr<ControlLoop> _control_loop;
-    
-    double  _dt {0.01};
-    double _pitch;
-
+    double _dt {0.01};
+    double _pitch {0.0};
     QTimer _simulation_timer;
-    
-    virtual void update();
+
+    void update();
 };
 
 #endif // FLIGHT_SIMULATION_H
