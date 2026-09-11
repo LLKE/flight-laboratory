2701d66efa525ff19e078a01f65ccc1ebbbbdd07
Laurens Keuker <laurens.keuker@gmail.com>
2026-09-11 18:32:25 +0200

    feat (flight simulation): allow flight simulation to be instantiated in qml and to provide dt and control loop type.

--- src/flight_simulation/flight_simulation.cpp
+++ src/flight_simulation/flight_simulation.cpp
@@ -1,13 +1,49 @@
 #include "flight_simulation/flight_simulation.h"
 
+#include "control/loops/pid_control_loop.h"
 
-FlightSimulation::FlightSimulation(std::unique_ptr<ControlLoop> loop, double dt, QObject *parent)
-: _control_loop(std::move(loop)), _dt(dt), QObject(parent)
+FlightSimulation::FlightSimulation(QObject *parent)
+    : QObject(parent)
 {
     _simulation_timer.setInterval(_dt * 1000);
     connect(&_simulation_timer, &QTimer::timeout, this, &FlightSimulation::update);
+    makeControlLoop(_controlLoopType);
 }
 
-void FlightSimulation::update() {
-    _control_loop->update(_dt);
+void FlightSimulation::setDt(double dt)
+{
+    if (_dt == dt) {
+        return;
+    }
+
+    _dt = dt;
+    _simulation_timer.setInterval(_dt * 1000);
+    emit dtChanged(_dt);
+}
+
+void FlightSimulation::setControlLoopType(const QString &type)
+{
+    if (_controlLoopType == type) {
+        return;
+    }
+
+    _controlLoopType = type;
+    makeControlLoop(type);
+    emit controlLoopTypeChanged();
+}
+
+void FlightSimulation::makeControlLoop(const QString &type)
+{
+    if (type == "pid") {
+        _control_loop = std::make_unique<pidControlLoop>();
+    } else {
+        _control_loop = std::make_unique<pidControlLoop>();
+    }
+}
+
+void FlightSimulation::update()
+{
+    if (_control_loop) {
+        _control_loop->update(_dt);
+    }
 }
--- src/flight_simulation/flight_simulation.h
+++ src/flight_simulation/flight_simulation.h
@@ -5,6 +5,9 @@
 
 #include <QObject>
 #include <QTimer>
+#include <QString>
+
+#include <QtQml/qqmlregistration.h>
 
 #include "src/control/loops/control_loop.h"
 
@@ -12,25 +15,34 @@ class FlightSimulation : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(double pitch READ pitch NOTIFY pitchChanged)
+    Q_PROPERTY(QString controlLoopType READ controlLoopType WRITE setControlLoopType NOTIFY controlLoopTypeChanged)
+    Q_PROPERTY(double dt READ dt WRITE setDt NOTIFY dtChanged)
+    QML_ELEMENT
 
 public:
-    explicit FlightSimulation(
-        std::unique_ptr<ControlLoop> loop,
-        double dt,
-        QObject *parent = nullptr);
+    explicit FlightSimulation(QObject *parent = nullptr);
 
     double pitch() const { return _pitch; }
+    double dt() const { return _dt; }
+    QString controlLoopType() const { return _controlLoopType; }
+
+    void setDt(double dt);
+    void setControlLoopType(const QString &type);
 
 signals:
     void pitchChanged(double newPitch);
+    void dtChanged(double newDt);
+    void controlLoopTypeChanged();
 
 private:
+    void update();
+    void makeControlLoop(const QString &type);
+
     std::unique_ptr<ControlLoop> _control_loop;
     double _dt {0.01};
     double _pitch {0.0};
+    QString _controlLoopType {"pid"};
     QTimer _simulation_timer;
-
-    void update();
 };
 
 #endif // FLIGHT_SIMULATION_H
